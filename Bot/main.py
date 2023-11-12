42b722f6f0d79c9f6ec0c945972a68076b4160a5
vaibhavlvl1 <110812464+vaibhavlvl1@users.noreply.github.com>
2023-11-12 10:11:11 +0530

    Add files via upload

--- Bot/main.py
+++ Bot/main.py
@@ -1,29 +1,32 @@
 # imports
 import os
+
+# import pdf reader
 import pdfplumber
 
-# from openpyxl import load_workbook
+# import excel writer
 import xlwt
 
 
 # excel utilities
-
-
 style1 = xlwt.easyxf("font:bold 1,color blue;")
 style2 = xlwt.easyxf("font:bold 1,color red;")
 style3 = xlwt.easyxf("font:bold 1,color green;")
 
+# create workbook and sheet
 wb = xlwt.Workbook()
 sheet1 = wb.add_sheet("Result")
+
+# write headings to sheet with style1 on row = 1
 sheet1.write(1, 0, "FileName", style1)
 sheet1.write(1, 1, "Keyword", style1)
 sheet1.write(1, 2, "Result", style1)
 
 # get file list
-
 file_list = [file for file in os.listdir() if file.endswith(".pdf")]
 
 
+# main function
 def ehe():
     row = 2
     for file in file_list:
@@ -49,11 +52,15 @@ def ehe():
             if key in content:
                 sheet1.write(row, 1, key)
                 sheet1.write(row, 2, True, style3)
+                continue
             else:
                 sheet1.write(row, 1, key)
                 sheet1.write(row, 2, False, style2)
+
             row += 1
     wb.save("xlwt result.xls")
 
+
+# run main function
 
 ehe()
